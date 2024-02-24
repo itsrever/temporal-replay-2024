9303cdfd4a4da72a3b2b96362977d498b5de13da
Eric Valls <eric.valls@itsrever.com>
2024-02-24 13:19:00 +0100

    .

--- Makefile
+++ Makefile
@@ -8,4 +8,7 @@ init:
 	go run ./cmd/main.go
 
 run-wf:
-	temporal workflow start --task-queue greetings --type Greet --input '"Eric"'
+	temporal workflow start --task-queue greetings --type Greet
+
+
+# temporal workflow describe --workflow-id 15e9c0c6-48ce-42a0-a897-1f27a67cb9b0
--- cmd/main.go
+++ cmd/main.go
@@ -33,7 +33,7 @@ func main() {
 	}
 }
 
-func Greet(ctx workflow.Context, name string) (string, error) {
+func Greet(ctx workflow.Context, name string) error {
 	ao := workflow.ActivityOptions{
 		StartToCloseTimeout: 10 * time.Second,
 		RetryPolicy: &temporal.RetryPolicy{
@@ -43,31 +43,60 @@ func Greet(ctx workflow.Context, name string) (string, error) {
 	}
 	ctx = workflow.WithActivityOptions(ctx, ao)
 
-	TryActivity(ctx, name)
+	TryThings(ctx, func() error {
+		// This needs to be idempotent
+		err := workflow.ExecuteActivity(ctx, FirstActivityReference).Get(ctx, nil)
+		if err != nil {
+			return err
+		}
 
-	return "Hello, " + name, nil
+		return workflow.ExecuteActivity(ctx, SecondActivityReference).Get(ctx, nil)
+	})
+
+	// TryThings(ctx, func() error {
+	// 	return workflow.ExecuteActivity(ctx, SecondActivityReference).Get(ctx, nil)
+	// })
+
+	SetCompleted(ctx)
+
+	return nil
 }
 
-func TryActivity(ctx workflow.Context, name string) {
-	err := workflow.ExecuteActivity(ctx, SayGreetingReference, name).Get(ctx, nil)
+func TryThings(ctx workflow.Context, c func() error) {
+	err := c()
 	if err == nil {
 		return
 	}
 
 	isRetry := OnActivityFailed(ctx)
 	if isRetry {
-		TryActivity(ctx, name)
+		TryThings(ctx, c)
 	}
 }
 
-func OnActivityFailed(ctx workflow.Context) bool {
+func SetOnHold(ctx workflow.Context) {
 	attributes := map[string]interface{}{
 		"ReverStatus": "ON_HOLD",
 	}
-	// This won't persist search attributes on server because commands are not sent to server,
-	// but local cache will be updated.
 	_ = workflow.UpsertSearchAttributes(ctx, attributes)
+}
 
+func SetRunning(ctx workflow.Context) {
+	attributes := map[string]interface{}{
+		"ReverStatus": "RUNNING",
+	}
+	_ = workflow.UpsertSearchAttributes(ctx, attributes)
+}
+
+func SetCompleted(ctx workflow.Context) {
+	attributes := map[string]interface{}{
+		"ReverStatus": "COMPLETED",
+	}
+	_ = workflow.UpsertSearchAttributes(ctx, attributes)
+}
+
+func OnActivityFailed(ctx workflow.Context) bool {
+	SetOnHold(ctx)
 	isRetry := false
 	selector := workflow.NewSelector(ctx)
 	selector.AddReceive(workflow.GetSignalChannel(ctx, "retry"), func(c workflow.ReceiveChannel, more bool) {
@@ -79,14 +108,21 @@ func OnActivityFailed(ctx workflow.Context) bool {
 		isRetry = false
 	})
 	selector.Select(ctx)
+	SetRunning(ctx)
 
 	return isRetry
 }
 
-var SayGreetingReference = (&Activities{}).SayGreeting
+var FirstActivityReference = (&Activities{}).FirstActivity
+var SecondActivityReference = (&Activities{}).SecondActivity
 
 type Activities struct{}
 
-func (a *Activities) SayGreeting(name string) (string, error) {
-	return "", errors.New("failed to say greeting")
+func (a *Activities) FirstActivity() error {
+	// return errors.New("failed to say greeting")
+	return nil
+}
+
+func (a *Activities) SecondActivity() error {
+	return errors.New("failed to say greeting")
 }
